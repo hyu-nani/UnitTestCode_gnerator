2f34a3b41bfbf416d55a1139728c411c5bf73736
hyu-nani <cube_h2@naver.com>
2021-10-23 22:50:57 +0900

    test

--- TCgenerator.py
+++ TCgenerator.py
@@ -7,24 +7,26 @@ import xlwings as xw
 import shutil
 from datetime import date
 
+
 print("\n┌───────────────────────────────────────────┐")
 print("│         Unit TC report generator          │")
 print("└───────────────────────────────────────────┘\n")
 print("엑셀에 기입하려고 하는 CSV 파일은 csvFolder 에 넣고")
-print("테스트보고서와 Test_Result폴더는 testReport 폴더넣어주세요")
+print("테스트보고서와 개별테스트보고서는 테스트보고서 내보내기 디렉토리 경로를")
+print("testReport폴더로 지정하고 내보내기 하시면 됩니다")
 print("readme.txt 를 수정하시면 세부사항을 기입할 수 있습니다.\n")
 input("진행할려면 엔터를 눌러주세요...\n\n")
 print("FLOWINUS.")
 
 xl_file     =   'VW_AQ_EOP_SWUTR_CR.xlsx'   #CT 빈파일
-path        =   'csvFolder/'                #csv 파일 모음폴더
+CSVpath     =   'csvFolder/'                #csv 파일 모음폴더
 Tester      =   ''                          #테스터
 fileName    =   ''                          #파일이름
 TCnumber    =   ''                          #테스트넘버
 TCresultName=   ''                          #
 
 personal    =   []            #readme파일 읽기위한 빈통
-file_list   =   os.listdir(path)
+file_list   =   os.listdir('testReport/')
 csv_list    =   []            #csv 파일 리스트
 testCaseID  =   []            #TC ID
 note        =   []            #csv 파일 내용 문자열
@@ -51,6 +53,8 @@ print('파일 읽어오는중..')
 txt = open('readme.txt', 'r',encoding='utf-8-sig')
 excelApp1 = win32com.client.dynamic.Dispatch('Excel.Application')
 excelApp1.Quit()
+TCresultName = str('test_' + file_list[0] + '.xlsx')
+projectName = file_list[0]
 for i in txt:
     personal.append(i.split(':'))
 Tester = personal[0][1].strip('\n')
@@ -60,9 +64,9 @@ if personal[1][1] != '\n':
 else:
     date = str(date.today())
 TCnumber = personal[3][1].strip('\n').strip(' ')
-TCresultName = str('test_' + personal[4][1].strip('\n').replace(' ','') + '.xlsx')
 txt.close()
 
+file_list   =   os.listdir(CSVpath)
 if len(file_list) == 0:
     print("There is no file\n")
 else:
@@ -72,7 +76,7 @@ else:
         if file_list[i].find('.csv') > 0:
             csv_list.append(file_list[i].replace('.csv',''))
     for i in range(len(csv_list)):
-        data = csv.reader(open(str(path+csv_list[i]+'.csv'),encoding='cp949'))
+        data = csv.reader(open(str(CSVpath+csv_list[i]+'.csv'),encoding='cp949'))
         for j in data:
             note.append(j)
         name = note[1][0].strip('test name:').split('_test')
@@ -116,13 +120,13 @@ else:
         swddsCode[i] = swddsCode[i].strip('[SWDDS.').strip(']')
     txt.close()
     print("테스트 보고서 유무확인")
-    file_list = os.listdir('testReport/')
+    file_list = os.listdir(str('testReport/'+projectName+'/'))
     if len(file_list) > 0:
         for i in range(len(file_list)):
             if file_list[i] == TCresultName:
                 fileOnOff = 1
                 print("발견")
-                resultBook = xw.Book(str('testReport/'+TCresultName))
+                resultBook = xw.Book(str('testReport/'+projectName+'/'+TCresultName))
                 sheet = resultBook.sheets['Sheet0']
                 i = 1
                 data = str(sheet[str('A1')].value)
@@ -133,6 +137,8 @@ else:
                     data = str(sheet['A' + str(i)].value)
                     if i > 5000:
                         break
+                    elif i%50==0:
+                        print('.',end='')
                 print('')
                 while 0 > str(sheet['B' + str(i)].value).find('총 함수 개수'):
                     i = i + 1
@@ -176,8 +182,6 @@ else:
         fileOnOff = 0
         print('파일없음')
 
-    excelApp1 = win32com.client.dynamic.Dispatch('Excel.Application')
-    excelApp1.Quit()
     #엑셀자동생성
     file_list = os.listdir()
     for i in range(len(file_list)):
@@ -187,9 +191,9 @@ else:
     shutil.copy(str('resource/'+xl_file), xl_file) # 결과 출력을 위한 빈파일 생성
     os.rename(xl_file, xl_file.strip('.xlsx') + '-00' + TCnumber +'.xlsx')
     xlbook = xw.Book(xl_file.strip('.xlsx') + '-00' + TCnumber +'.xlsx')
+    print('출력',end='')
     sheet = xlbook.sheets['filename.c']
     Ycell = 0
-    print('출력')
     for i in range(len(functionName)):
         if functionNum[i] == 1:
             sheet.range('C' + str(5 + Ycell)).value = str('SWDDS.' + swddsCode[i])      # SWDDS출력
@@ -214,6 +218,7 @@ else:
             sheet.range('Q' + str(5 + Ycell)).value = '-'                               # -
             sheet.range('H' + str(5 + Ycell)).value = '-'                               # -
             Ycell = Ycell + 1
+            print('.',end='')
         else:
             for j in range(functionNum[i]):
                 sheet.range('C' + str(5 + Ycell)).value = str('SWDDS.' + swddsCode[i]+'_')+str(j+1)     # SWDDS출력
@@ -238,36 +243,39 @@ else:
                 sheet.range('Q' + str(5 + Ycell)).value = '-'                                               # -
                 sheet.range('H' + str(5 + Ycell)).value = '-'                                               # -
                 Ycell = Ycell + 1
+                print('.', end='')
     xlbook.sheets['filename.c'].name = fileName
+    print()
     print("생성완료")
     xlbook.save()
     xlbook.app.quit()
     char = input("Test Result내 파일 명을 변경하시겠습니까? y/n :")
     if char == 'y':
-        file_list = os.listdir('testReport/Test_Result/')
+        file_list = os.listdir(str('testReport/'+projectName+'/Test_Result/'))
         print("기존파일제거")
         for i in range(len(file_list)):
             if file_list[i].find('SWUTS') >= 0:
-                os.remove('testReport/Test_Result/' + file_list[i])
+                os.remove(str('testReport/'+projectName+'/Test_Result/'+ file_list[i]))
                 print('remove:'+file_list[i])
-        print("Test Result 파일명 변경중.")
-        file_list = os.listdir('testReport/Test_Result/')
+        print("Test Result 파일명 변경중.",end='')
+        file_list = os.listdir(str('testReport/'+projectName+'/Test_Result/'))
         for i in range(len(functionName)):
             for j in range(len(file_list)):
                 for k in range(int(functionNum[i])):
-                    #print(str(functionName[i] + '_test' + str(k)+'.xls'))
                     if file_list[j] == str(functionName[i]+'_test'+str(k)+'.xls'):
+                        print('.', end='')
                         if functionNum[i] == 1:
-                            os.rename('testReport/Test_Result/'+functionName[i]+'_test'+str(k)+'.xls','testReport/Test_Result/SWUTS-F.'+swddsCode[i]+'.xls')
+                            os.rename(str('testReport/'+projectName+'/Test_Result/'+functionName[i]+'_test'+str(k)+'.xls'),str('testReport/'+projectName+'/Test_Result/SWUTS-F.'+swddsCode[i]+'.xls'))
                         else:
-                            os.rename('testReport/Test_Result/' + functionName[i] + '_test' + str(k) + '.xls','testReport/Test_Result/SWUTS-F.' + swddsCode[i] + '_' + str(k + 1) + '.xls')
+                            os.rename(str('testReport/'+projectName+'/Test_Result/' + functionName[i] + '_test' + str(k) + '.xls'),str('testReport/'+projectName+'/Test_Result/SWUTS-F.' + swddsCode[i] + '_' + str(k + 1) + '.xls'))
+        print()
         char = input("이름이 변경되지 않은 파일을 제거하시겠습니까? y/n :");
         if char == 'y':
-            file_list = os.listdir('testReport/Test_Result/')
+            file_list = os.listdir('testReport/'+projectName+'/Test_Result/')
             print("제거중.")
             for i in range(len(file_list)):
                 if file_list[i].find('SWUTS') < 0:
-                    os.remove('testReport/Test_Result/'+file_list[i])
+                    os.remove('testReport/'+projectName+'/Test_Result/'+file_list[i])
                     print("remove:"+file_list[i])
     print("완료")
 
--- readme.txt
+++ readme.txt
@@ -1,17 +1,16 @@
-Tester : Bae HyeonHan
-date : 2021-09-21
+Tester : BaeHyeonHan
+date : 2021-09-23
 file_name: apCurSig.c
-Test_num: 41
-Test_name: UnitTest
+Test_num: 40
 
 //////////////////////////////////////////////////////////////
 
 file_name: TC내 파일 이름
 Test_num: 파일이름 가장 마지막에 들어갑니다.
-Test_name: 테스트결과 파일 읽어내는데 사용됩니다 (테스트보고서 이름 :test_이름.xlsx)
-
 날짜는 없으면 현재날짜로 들어갑니다
 
 실행도중 엑셀이 켜지면 끝날 때까지 기다려주세요
 
-csvFolder 안에 CSV 파일을 모두 넣고 테스트보고서와 개별테스트보고서 폴더 Test_Result는 testReport에 넣으시면 됩니다.
+csvFolder 안에 CSV 파일을 모두 넣고 
+테스트보고서와 개별테스트보고서는 테스트보고서 내보내기 디렉토리 경로를 testReport폴더로 지정하고
+내보내기 하시면 됩니다
